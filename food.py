f9421cca2ee757f33c415729d243ac7a08dc32da
it21104008 <87991955+it21104008@users.noreply.github.com>
2024-02-20 13:05:42 +0530

    Add files via upload

---
+++ food.py
@@ -0,0 +1,10 @@
+menu = ["Rice", "chicken", "bun", "noodles"]
+
+food = input("Can i get :")
+
+if food in menu:
+    print("yes!!! you can take")
+else:
+    print("sorry! we dont have")
+
+print("thank you....")
